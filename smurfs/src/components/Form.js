530b1c0d11069dc93e3dd8b6d769fbe9ef236f6f
shaunorpen <shaunorpen@users.noreply.github.com>
2019-10-11 10:02:13 +0100

    Working on Form

--- smurfs/src/components/Form.js
+++ smurfs/src/components/Form.js
@@ -1,15 +1,37 @@
-import React from 'react';
-// import { addSmurf } from '../state/actionCreators';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addSmurf } from '../state/actionCreators';
 
 export default function Form () {
+    const [smurf, setSmurf] = useState({
+        name: '',
+        age: 0,
+        height: 0,
+    });
+
+    const dispatch = useDispatch();
+
+    const handleChange = (event) => {
+        setSmurf({
+            ...smurf,
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    const handleSubmit = (event) => {
+        debugger
+        event.preventDefault();
+        dispatch(addSmurf(smurf));
+    }
+
     return (
         <div>
             <h2>Add a Smurf!</h2>
             <form>
-                <input type='text' placeholder='Name' />
-                <input type='number' placeholder='Height' />
-                <input type='number' placeholder='Age' />
-                <button>Submit</button>
+                <input type='text' placeholder='Name' name='name' value={smurf.name} onChange={handleChange} />
+                <input type='number' placeholder='Age' name='age' value={smurf.age} onChange={handleChange} />
+                <input type='number' placeholder='Height' name='height' value={smurf.height} onChange={handleChange} />
+                <button onClick={handleSubmit}>Submit</button>
             </form>
         </div>
     )
